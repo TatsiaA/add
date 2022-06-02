80e167f20ff24f994c6ffb88ec3be097df3b6ab0
TatsiaA <taniokk@mail.ru>
2022-06-03 00:45:58 +0300

    add view for categories

--- Asia_chronicle/index.js
+++ Asia_chronicle/index.js
@@ -6,30 +6,27 @@ let categories = 'categories';
 function getInfo(url, sbj) {
   fetch(`${url}${sbj}`)
   .then((response) => {
-    return response.json();
+    if (response.status !== 200) {
+      return Promise.reject(new Error(response.statusText))
+    }
+    return Promise.resolve(response)
   })
-  .then((data) => {
-    data[sbj].forEach(element => {
-      let row = `<a href="${url}${sbj}/${element}">${element}</a>`;
-      document.getElementById(`${sbj}`).insertAdjacentHTML('beforeend', row);
-    });
-  })
-  .catch((error) => {
-    handleError(error);
-  })
-}
-
-function getInfo1(url, sbj) {
-  fetch(`${url}${sbj}`)
-  .then(errorHandler)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     data[sbj].forEach(element => {
-      let row = `<a href="${element.Link}">${element.API}</a>`;
+      let row = !element.Link
+        ? `<p class="category">${element}</p>`
+        : `<a href="${element.Link}">${element.API}</a>`;
       document.getElementById(`${sbj}`).insertAdjacentHTML('beforeend', row);
     });
+    let category = document.getElementsByClassName('category');
+    if(category) {
+      Array.prototype.forEach.call(category, el => {
+        el.addEventListener('click', showCategoryInfo)
+      });
+    }
   })
   .catch((error) => {
     handleError(error);
@@ -37,39 +34,64 @@ function getInfo1(url, sbj) {
 }
 
 getInfo(baseLink, categories);
-getInfo1(baseLink, entries);
+getInfo(baseLink, entries);
+
+function showCategoryInfo() {
+  let oldRequest = document.querySelector('.request-info');
+  if (oldRequest) {
+    oldRequest.textContent = this.textContent;
+    Array.prototype.forEach.call(document.querySelectorAll('.api-name'), el => el.remove());
+  } else {
+    let result = document.createElement('p');
+    result.className = 'request-info';
+    result.textContent = this.textContent;
+    document.querySelector('script').insertAdjacentElement('beforebegin', result);
+  }
+  getCategoryInfo(baseLink, entries);
+}
+
+function getCategoryInfo(url, sbj) {
+  fetch(`${url}${sbj}`)
+  .then((response) => {
+    if (response.status !== 200) {
+      return Promise.reject(new Error(response.statusText))
+    }
+    return Promise.resolve(response)
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data[sbj].forEach(element => {
+      if (element.Category === document.querySelector('.request-info').textContent) {
+        let chosen = document.createElement('span');
+        chosen.className = 'api-name';
+        chosen.textContent = '\n' + element.API + ' (' + element.Description + ')';
+        document.querySelector('script').insertAdjacentElement('beforebegin', chosen);
+      }
+    });
+  })
+  .catch((error) => {
+    handleError(error);
+  })
+}
 
 function handleError(error) {
-  if (error instanceof TypeError) {
-    console.log('TypeError: ', error.message);
-  } else if (error instanceof RangeError) {
-    console.log('RangeError: ', error.message);
-  } else if (error instanceof EvalError) {
-    console.log('EvalError: ', error.message);
-  } else if (error instanceof ReferenceError) {
-    console.log('ReferenceError: ', error.message);
-  } else {
-    throw error.status;
-  }
+    if (error instanceof TypeError) {
+      console.log('TypeError: ', error.message);
+    } else if (error instanceof RangeError) {
+      console.log('RangeError: ', error.message);
+    } else if (error instanceof EvalError) {
+      console.log('EvalError: ', error.message);
+    } else if (error instanceof ReferenceError) {
+      console.log('ReferenceError: ', error.message);
+    } else {
+      throw error.status;
+    }
 }
 
-function errorHandler(response) {
-
-  if (!response.ok) {
-    console.log('response');
-      if (response.status === '401' || response.status === '404')
-          console.log(`Sorry, but there is ${response.status} error: ${response.statusText}`);
-      throw Error(response.statusText);
-  }
-  return response;
-}
-
-function showTitle() {
-  document.getElementById("entries").classList.toggle("show");
-}
-
-function showCategory() {
-  document.getElementById("categories").classList.toggle("show");
+function show(inner) {
+  document.getElementById(`${inner}`).classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
