b4ef635686e9a45b71f159277993b6a496dcc1fb
TatsiaA <taniokk@mail.ru>
2022-06-22 16:18:19 +0300

    corrected

--- Asia_chronicle/index.js
+++ Asia_chronicle/index.js
@@ -1,150 +1,143 @@
 const baseLink = 'https://api.publicapis.org/';
 
-let entries = 'entries';
-let categories = 'categories';
+const entries = 'entries';
+const categories = 'categories';
 
-let titlesFirstLetters = new Set();
+const titlesFirstLetters = new Set();
 let dataEntries = [];
 let dataCategories = [];
 
-function getDataCategory() {
-  fetch(`${baseLink}${categories}`)
-  .then((response) => {
+getDataCategory().then(() => workWithCategories(dataCategories));
+getDataTitle().then(() => workWithTitles(dataEntries));
+
+async function getDataCategory() {
+    let response = await fetch(`${baseLink}${categories}`);
     if (response.status !== 200) {
-      return Promise.reject(new Error(response.statusText))
+        return Promise.reject(new Error(response.statusText))
     }
-    return Promise.resolve(response)
-  })
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    dataCategories = data[categories];
-    dataCategories.forEach(element => {
-        row = `<p class="category">${element}</p>`;
+    let commits = await response.json();
+    dataCategories = [...commits[categories]];
+}
+
+function workWithCategories(arr) {
+    arr.forEach(element => {
+        let row = `<p class="category">${element}</p>`;
         document.getElementById(categories).insertAdjacentHTML('beforeend', row);
     });
     let category = document.getElementsByClassName('category');
     if (category) {
-      Array.prototype.forEach.call(category, el => {
-        el.addEventListener('click', showCategoryInfo)
-      });
+        Array.prototype.forEach.call(category, el => {
+            el.addEventListener('click', showCategoryInfo)
+        });
     }
-  })
-  .catch((error) => {
-    console.error(`Fetch problem: ${error.message}`)
-  })
 }
 
-function getDataTitle() {
-  fetch(`${baseLink}${entries}`)
-  .then((response) => {
+async function getDataTitle() {
+    let response = await fetch(`${baseLink}${entries}`);
     if (response.status !== 200) {
-      return Promise.reject(new Error(response.statusText))
+        return Promise.reject(new Error(response.statusText))
     }
-    return Promise.resolve(response)
-  })
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    dataEntries = data[entries];
+    let commits = await response.json();
+    dataEntries = [...commits[entries]];
+}
+
+function workWithTitles(arr) {
     // get titlesFirstLetters unique collection
-    dataEntries.forEach(element => {
-        titlesFirstLetters.add((element.API).slice(0, 1));
+    arr.forEach(element => {
+        titlesFirstLetters.add((element.API).slice(0, 1).toUpperCase());
     });
     // transform to array and sort letters collection
     const sortedTitlesFirstLetters = Array.from(titlesFirstLetters).sort();
     // insert rows with letters in dropdown menu
     sortedTitlesFirstLetters.forEach(letter => {
-      row = `<p class="api-title">${letter}</p>`;
-      document.getElementById('entries').insertAdjacentHTML('beforeend', row);
+        let row = `<p class="api-title">${letter}</p>`;
+        document.getElementById('entries').insertAdjacentHTML('beforeend', row);
     })
-  
+    // add listener to dropdown title
     let apiTitle = document.getElementsByClassName('api-title');
     if (apiTitle) {
-      Array.prototype.forEach.call(apiTitle, el => {
-        el.addEventListener('click', showApiInfo)
-      });
+        Array.prototype.forEach.call(apiTitle, el => {
+            el.addEventListener('click', showApiInfo)
+        });
     }
-  })
-  .catch((error) => {
-    console.error(`Fetch problem: ${error.message}`)
-  })
 }
 
-getDataCategory();
-getDataTitle();
-
 function showCategoryInfo() {
-  let oldRequest = document.querySelector('.request-info');
-  if (oldRequest) {
-    oldRequest.textContent = this.textContent;
-    Array.prototype.forEach.call(document.querySelectorAll('.api-name'), el => el.remove());
-  } else {
-    let result = document.createElement('p');
-    result.className = 'request-info';
-    result.textContent = this.textContent;
-    document.querySelector('script').insertAdjacentElement('beforebegin', result);
-  }
-  getCategoryInfo(baseLink, entries);
+    // remove previous responses info
+    if (document.querySelector('.request-title')) {
+        document.querySelector('.request-title').remove();
+        Array.prototype.forEach.call(document.querySelectorAll('.api-title-name'), el => el.remove());
+    }
+
+    if (document.querySelector('.request-info')) {
+        document.querySelector('.request-info').textContent = this.textContent;
+        Array.prototype.forEach.call(document.querySelectorAll('.api-name'), el => el.remove());
+    } else {
+        // create and render new response info title
+        let result = document.createElement('p');
+        result.className = 'request-info';
+        result.textContent = `The list of APIs in category ${this.textContent}`;
+        document.querySelector('script').insertAdjacentElement('beforebegin', result);
+    }
+
+    // create and render new response info items
+    let chosenCategory = this.textContent;
+    dataEntries.forEach(element => {
+        if (element.Category === chosenCategory) {
+            let chosen = document.createElement('p');
+            chosen.className = 'api-name';
+            chosen.textContent = ` (${element.Description})`;
+            chosen.insertAdjacentHTML("afterbegin", `<a href=${element.Link}>${element.API}</a>`);
+            document.querySelector('script').insertAdjacentElement('beforebegin', chosen);
+        }
+    })
 }
 
 function showApiInfo() {
-  console.log(1);
-  // let oldRequest = document.querySelector('.request-title');
-  // if (oldRequest) {
-  //   oldRequest.textContent = this.textContent;
-  //   Array.prototype.forEach.call(document.querySelectorAll('.api-name'), el => el.remove());
-  // // } else {
-  //   let result = document.createElement('p');
-  //   result.className = 'request-title';
-  //   result.textContent = this.textContent;
-  //   document.querySelector('script').insertAdjacentElement('beforebegin', result);
-  // }
-}
-
-function getCategoryInfo(url, service) {
-  fetch(`${url}${service}`)
-  .then((response) => {
-    if (response.status !== 200) {
-      return Promise.reject(new Error(response.statusText))
+    // remove previous responses info
+    if (document.querySelector('.request-info')) {
+        document.querySelector('.request-info').remove();
+        Array.prototype.forEach.call(document.querySelectorAll('.api-name'), el => el.remove());
     }
-    return Promise.resolve(response)
-  })
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    data[service].forEach(element => {
-      console.log(element.Category, document.querySelector('.request-info').textContent);
-      if (element.Category === document.querySelector('.request-info').textContent) {
-        let chosen = document.createElement('span');
-        chosen.className = 'api-name';
-        chosen.textContent = ` (${element.Description})`;
-        chosen.insertAdjacentHTML("afterbegin", `<a href=${element.Link}>${element.API}</a>`);
-        document.querySelector('script').insertAdjacentElement('beforebegin', chosen);
-      }
-    });
-  })
-  .catch((error) => {
-    console.error(`Fetch problem: ${error.message}`)
-  })
+
+    if (document.querySelector('.request-title')) {
+        document.querySelector('.request-title').textContent = this.textContent;
+        Array.prototype.forEach.call(document.querySelectorAll('.api-title-name'), el => el.remove());
+    } else {
+        // create and render new response info title
+        let result = document.createElement('p');
+        result.className = 'request-title';
+        result.textContent = this.textContent;
+        document.querySelector('script').insertAdjacentElement('beforebegin', result);
+    }
+
+    // create and render new response info items
+    let chosenTitleLetter = this.textContent;
+    dataEntries.forEach(entry => {
+        if ((entry.API).slice(0, 1).toUpperCase() === chosenTitleLetter) {
+            let chosenTitle = document.createElement('p');
+            chosenTitle.className = 'api-title-name';
+            chosenTitle.textContent = ` (${entry.Description})`;
+            chosenTitle.insertAdjacentHTML("afterbegin", `<a href=${entry.Link}>${entry.API}</a>`);
+            document.querySelector('script').insertAdjacentElement('beforebegin', chosenTitle);
+        }
+    })
 }
 
+// show the dropdown menu by click the button
 function show(inner) {
-  document.getElementById(`${inner}`).classList.toggle('show');
+    document.getElementById(`${inner}`).classList.toggle('show');
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    let dropdowns = document.getElementsByClassName('dropdown-content');
-    for (let i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+// Close the dropdown menu if the user clicks outside it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName('dropdown-content');
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
-  }
 }
